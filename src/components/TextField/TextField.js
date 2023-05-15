fa11a474c2dfd815cc76161fd7b3df8c0a341e64
RickHPotter </SOPHiA16>
2023-05-15 19:14:26 -0300

    giving life to the create-card form

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,10 +1,18 @@
 import "./TextField.css";
 
 export const TextField = (props) => {
+
   return (
     <div className="form__group">
       <label id="name">{props.label}</label>
-      <input type="text" name="username" placeholder={props.ph}/>
+      <input
+        required={props.required}
+        type="text"
+        name="username"
+        placeholder={props.ph}
+        value={''}
+        onChange={() => {}}
+      />
     </div>
   );
 };
